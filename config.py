4277deb5dfc08f2166e0d9f3911fe0f7235a66cf
AndrewOvvv <63587191+AndrewOvvv@users.noreply.github.com>
2021-05-14 22:41:34 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+settings = {
+    'token': 'NzQ5MTk5NTE1NjAwNTUxOTk2.X0ogaw.Ir4IkcNy6DXjxv_plyttfP_Gk-A',
+    'bot': 'Helper',
+    'id': 749199515600551996,
+    'prefix': '!'
+}
+
+#acc-687@mypython-297819.iam.gserviceaccount.com
+
+#emb.set_author(name = bot.user.name, icon_url = bot.user.avatar_url)
+#emb.set_image(url = 'https://c4.wallpaperflare.com/wallpaper/500/442/354/outrun-vaporwave-hd-wallpaper-thumb.jpg')
+#emb.set_thumbnail(url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlEM8D4Qh3wb2RHzTYq0FmiI5JdD4iLEXuyA&usqp=CAU')
